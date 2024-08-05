dd854408dd6321bf47dff01aef9a735cc6e02e17
Ahmed Rayan <61907734+ahmedrayan587@users.noreply.github.com>
2024-08-05 21:36:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,10 +32,18 @@ app.use("/message", messageRoutes);
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello, world!" });
 });
+app.use((req, res, next) => {
+  console.log(`Received request for ${req.method} ${req.url}`);
+  next();
+});
 
 
 mongoose
-  .connect(process.env.MONGO_URL)
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // Set a timeout of 5 seconds for initial connection
+  })
   .then(() => {
     console.log("DB Connected");
   })
